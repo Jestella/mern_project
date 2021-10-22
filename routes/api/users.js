c4785b9b429f96a2d3d39ff84a82d0a564f08bf6
jestella <jestellaa@gmail.com>
2021-10-21 22:44:21 -0400

    add auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs"); // encrypt password
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
@@ -54,8 +56,24 @@ router.post(
 
       await user.save();
 
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      ); //3600 = 1hr
+
       // Return jsonwebtoken   - when users register, they can login right away (frontend)
-      res.send("User registered");
+      // res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
